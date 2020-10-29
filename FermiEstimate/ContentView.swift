e98a1d4cf15e9459bf9e813e4de5aac2303eaa35
prosciutto0509 <cqcfoxhoundrexrayyy@yahoo.co.jp>
2020-10-29 11:41:07 +0900

    Initial Commit

---
+++ FermiEstimate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FermiEstimate
+//
+//  Created by 安田 悠麿 on 2020/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FermiEstimate/FermiEstimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FermiEstimateApp.swift
+//  FermiEstimate
+//
+//  Created by 安田 悠麿 on 2020/10/29.
+//
+
+import SwiftUI
+
+@main
+struct FermiEstimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
